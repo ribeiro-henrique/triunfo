6e68c98e6a681b671d1b7286d74923c2971b0f9a
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-04-05 17:38:29 -0300

    stand by

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '',
+    cardAttr2: '',
+    cardAttr3: '',
+    cardImage: '',
+    cardRare: '',
+    cardTrunfo: false,
+  };
+
+  handleChange = (event) => {
+    const { target } = event;
+    const { value, name, type } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
     return (
       <div>
